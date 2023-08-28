53c33fa06d0170422dc55e037e4257d4eecba7ee
HunterKnott <hunterknott00@gmail.com>
2023-08-28 09:46:09 -0600

    File read copies year headers

--- Gas_Prices/gasprices.py
+++ Gas_Prices/gasprices.py
@@ -1,4 +1,13 @@
 '''Examines weekly gas prices from 1994 to 2012'''
 
-f = open("gas_prices.txt", "r")
-print(f.read())
+# f = open('gas_prices.txt', 'r')
+# print(f.read())
+
+with open('gas_prices.txt') as gas_file:
+    current_year = 0
+    new_year = 0
+    for line in gas_file:
+        new_year = line[line.index("-") + 4 :line.index(":")]
+        if current_year != new_year:
+            current_year = new_year
+            print(current_year + ":")
