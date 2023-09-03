f7cf235a05854ee36ecad56aee54d3eb555d344f
HunterKnott <hunterknott00@gmail.com>
2023-09-03 14:51:17 -0600

    All files are read into sets.

--- Comprehensions/comp.py
+++ Comprehensions/comp.py
@@ -2,21 +2,40 @@
 from collections import namedtuple
 
 parts = set()
-# Part = namedtuple('pno', ['pname', 'color', 'weight', 'city'])
 with open('parts.txt') as parts_file:
     lines = [line.strip() for line in parts_file.readlines()]
 headers = lines[1].split(',')
-print(headers)
-Part = namedtuple(headers[0], [headers[1], headers[2], headers[3]])
+Part = namedtuple('Part', [headers[0], headers[1], headers[2], headers[3], headers[4]])
 for line in lines[2:]:
     entry = line.split(',')
-    parts.add(Part(entry[0], [entry[1], entry[2], entry[3]]))
+    parts.add(Part(entry[0], entry[1], entry[2], entry[3], entry[4]))
 
 projects = set()
+with open('projects.txt') as projects_file:
+    lines = [line.strip() for line in projects_file.readlines()]
+headers = lines[1].split(',')
+Project = namedtuple('Project', [headers[0], headers[1], headers[2]])
+for line in lines[2:]:
+    entry = line.split(',')
+    projects.add(Project(entry[0], entry[1], entry[2]))
 
 suppliers = set()
+with open('suppliers.txt') as suppliers_file:
+    lines = [line.strip() for line in suppliers_file.readlines()]
+headers = lines[1].split(',')
+Supplier = namedtuple('Supplier', [headers[0], headers[1], headers[2], headers[3]])
+for line in lines[2:]:
+    entry = line.split(',')
+    suppliers.add(Supplier(entry[0], entry[1], entry[2], entry[3]))
 
-entries = set()
+orders = set()
+with open('spj.txt') as spj_file:
+    lines = [line.strip() for line in spj_file.readlines()]
+headers = lines[1].split(',')
+Order = namedtuple('Order', [headers[0], headers[1], headers[2], headers[3]])
+for line in lines[2:]:
+    entry = line.split(',')
+    orders.add(Order(entry[0], entry[1], entry[2], entry[3]))
 
 def main():
     pass
