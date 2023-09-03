e147b5a114ce9b99941cd73fd8192967e3f58c8c
HunterKnott <hunterknott00@gmail.com>
2023-09-03 17:34:52 -0600

    First several comp queries completed.

--- Comprehensions/comp.py
+++ Comprehensions/comp.py
@@ -38,7 +38,29 @@ for line in lines[2:]:
     orders.add(Order(entry[0], entry[1], entry[2], entry[3]))
 
 def main():
-    pass
+    # 0. Get names of all suppliers for projects in London
+    london_projs = {j.jno for j in projects if j.city == 'London'} # Get list of all London jno's
+    london_supp_ids = {r.sno for r in orders if r.jno in london_projs}
+    london_supps = {s.sname for s in suppliers if s.sno in london_supp_ids}
+    
+    # 1. Get names of all suppliers that supply bolts.
+    bolt_parts = {p.pno for p in parts if p.pname == 'Bolt'}
+    bolt_supp_ids = {q.sno for q in orders if q.pno in bolt_parts}
+    bolt_supps = {s.sname for s in suppliers if s.sno in bolt_supp_ids}
+    print(bolt_supps)
+
+    # 2. Get names of all suppliers that supply blue parts.
+    blue_parts = {p.pno for p in parts if p.color == 'Blue'}
+    blue_supp_ids = {q.sno for q in orders if q.pno in blue_parts}
+    blue_supps = {s.sname for s in suppliers if s.sno in blue_supp_ids}
+    print(blue_supps)
+
+    # 3. Get names of all suppliers not used in Athens projects
+    
+
+    # 4. Get names and colors of all parts not used in Oslo
+    # 5. Get pairs of names of all suppliers that are located in the same city.
+    # 6. Print all suppliers out by city
 
 if __name__ == '__main__':
     main()
