333a2d7f26e2ac367c24b12007a405a5a6a58da8
HunterKnott <hunterknott00@gmail.com>
2023-09-10 23:22:19 -0600

    Finished project, output printed and written.

--- Decorators/dec.py
+++ Decorators/dec.py
@@ -11,19 +11,27 @@ def track(f):
         cache[key] = result
         f.count += 1
         wrapper.count = f.count
-        print(key)
+        print(key + ' found in cache')
         return result
     return wrapper
 
-# def log():
-#     def wrapper():
-#         logfile = open('log.txt', 'a')
+def log(f):
+    def wrapper(*args, **kwargs):
+        with open('log.txt', 'a') as logfile:
+            logfile.write(f'{f.__name__}{args, kwargs}')
+            result = f(*args, **kwargs)
+            logfile.write(f'Result: {result}\n')
+        return result
+    return wrapper
+
+@track
+@log
+def fib(n):
+    return n if n in (0,1) else fib(n-1) + fib(n-2)
 
 def main():
-    @track
-    # @log
-    def fib(n):
-        return n if n in (0,1) else fib(n-1) + fib(n-2)
+    with open('log.txt', 'w') as logfile:
+        logfile.write('')
     print(fib(10), 'calls =', fib.count)
 
 if __name__ == "__main__":
