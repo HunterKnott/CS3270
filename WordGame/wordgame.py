0e50b16867d2297f6c10f99265991e837abda4d4
HunterKnott <hunterknott00@gmail.com>
2023-10-04 14:09:29 -0600

    Correct guesses are recorded in spaces dict

--- WordGame/wordgame.py
+++ WordGame/wordgame.py
@@ -32,27 +32,23 @@ def main(length_range = None):
 
     sub_words.sort(key=len)
     sub_words_grouped = {key: list(group) for key, group in groupby(sub_words, key=len)}
-
-    # Print all permutations
-    # print(f'The word is {word}')
-    # for length, words_group in sub_words_grouped.items():
-    #     print(f'Words of length {length}:')
-    #     [print(word) for word in words_group]
     
     # Print scrambled word
     random.shuffle(word_chars)
     print('\n' + ''.join(word_chars) + ':\n')
     
-    word_spaces = []
+    word_spaces = {}
     for length, words_group in sub_words_grouped.items():
-        word_spaces.append('[' + ', '.join(["'" + '-' * length + "'" for word in words_group]) + ']')
-    for length in word_spaces:
-        print(length)
-    print(sub_words)
+        word_spaces[length] = ['-' * length for word in words_group]
+    {print(key, value) for key, value in word_spaces.items()}
+    print(sub_words_grouped)
 
+    # Take and process user guesses
     guess = input('\nEnter a guess: ')
     if guess in sub_words:
         print('Correct!')
+        word_spaces[len(guess)][sub_words_grouped.get(len(guess)).index(guess)] = guess
+        {print(key, value) for key, value in word_spaces.items()}
     elif guess == 'q':
         print('Exiting game...')
         exit()
