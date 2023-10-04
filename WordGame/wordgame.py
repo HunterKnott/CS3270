23546cd18204ae85f6bbd543ec758a21287af552
HunterKnott <hunterknott00@gmail.com>
2023-10-04 15:25:33 -0600

    Command line argument can be given for base word

--- WordGame/wordgame.py
+++ WordGame/wordgame.py
@@ -1,7 +1,8 @@
 '''Hunter Knott, CS 3270, Utah Valley University'''
-from itertools import groupby # Use to sort permutations by length
+import sys
+import itertools
 import random
-from collections import Counter # Use when checking for subset words
+import collections
 
 def read_words(file_name):
     try:
@@ -11,34 +12,40 @@ def read_words(file_name):
         print('The file ' + str(file_name) + ' does not exist')
     return lines
 
-def main(length_range = None):
-    if length_range:
-        min_length, max_length = map(int, length_range.split(','))
-    else:
-        min_length, max_length = 3, 15 # Default range
-
+def main():
     words = read_words('words.txt')
-    filter_words = [w for w in words if min_length <= len(w) <= max_length]
-    word = random.choice(filter_words)
+
+    if len(sys.argv) == 2 and sys.argv[1] in words:
+        word = sys.argv[1]
+        min_length, max_length = 3, len(word)
+    else:
+        if len(sys.argv) == 2: print('The given argument is not a valid word')
+        length_range = input('Enter the range for word length (min, max): ')
+        if length_range:
+            min_length, max_length = map(int, length_range.split(','))
+        else:
+            min_length, max_length = 3, 15 # Default range
+        filter_words = [w for w in words if min_length <= len(w) <= max_length]
+        word = random.choice(filter_words)
+
     word_chars = list(word)
 
-    from collections import Counter 
     sub_words = []
-    word_counter = Counter(word_chars)
+    word_counter = collections.Counter(word_chars)
     for w in words:
-        w_counter = Counter(w)
+        w_counter = collections.Counter(w)
         if all(w_counter[c] <= word_counter[c] for c in w) and min_length <= len(w) <= max_length:
             sub_words.append(w)
 
     sub_words.sort(key=len)
-    sub_words_grouped = {key: list(group) for key, group in groupby(sub_words, key=len)}
+    sub_words_grouped = {key: list(group) for key, group in itertools.groupby(sub_words, key=len)}
     
     word_spaces = {}
     for length, words_group in sub_words_grouped.items():
         word_spaces[length] = ['-' * length for word in words_group]
-    # {print(key, value) for key, value in word_spaces.items()}
     
     while word_spaces != sub_words_grouped:
+
         # Print scrambled word
         random.shuffle(word_chars)
         print('\n' + ''.join(word_chars) + ':\n')
@@ -56,10 +63,8 @@ def main(length_range = None):
         else:
             print('Sorry, try again')
 
-    print('\n')
-    {print(key, value) for key, value in word_spaces.items()}
+    print(sub_words)
     print('\nGood job! You got all the words.')
 
 if __name__ == "__main__":
-    length_range = input("Enter the range for word length (min, max): ")
-    main(length_range)
+    main()
