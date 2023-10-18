35c599aa5f3d28bd0fd8c3ff8f3fb5c0e0789119
HunterKnott <hunterknott00@gmail.com>
2023-10-17 20:36:12 -0600

    Initial commit with project requirements.

---
+++ Stats/drv.py
@@ -0,0 +1,23 @@
+'''Hunter Knott, ECE 3710, Utah Valley University'''
+# Discrete Random Variable 
+# P(X=1) = 0.3, P(X=0) = 0.7
+# Probability Mass Function: f(x) = P^x (1-P)^(1-x)
+# This is a Bernoulli Distribution
+
+# a. Find the mean (expected value) of X using the given pmf. This means that what do we get in the output, on the average.
+
+# b. Find the variance of the random variable X using the given pmf.
+
+# c. Now the engineer/scientist, starts measuring the output. He/she repeats the process 20 times. The outcomes are as follows:
+# X = [1 0 0 0 0 0 0 0 1 0 1 1 1 1 0 0 0 0 1 0]
+
+# c-1. Plot the histogram of data
+
+# c-2. Normalize the histogram and plot it.
+
+# c-3. Find the mean and variance of the collected samples. Find the probability of p(X=1) from the
+# collected measurements (is this the same as the actual probability P?). Compare the results with the actual
+# mean and variances. Are they different? Explain what you learned from this problem.
+
+# c-4) Plot the data with the approximated pmf (look at the actual pmf and instead of P use the probability
+# of p(X=1) you found from the data). Plot the approximated and the actual pmf.
