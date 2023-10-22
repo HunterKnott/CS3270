1d7a33d6457861b3682b495a455f0377c459e138
HunterKnott <hunterknott00@gmail.com>
2023-10-21 21:00:52 -0600

    Transferred HTML lines to Indexer file. Added capability to scan and display current directory.

--- Indexer/indexer.py
+++ Indexer/indexer.py
@@ -1,11 +1,61 @@
 '''Hunter Knott, CS 3270, Utah Valley University'''
 import os
 
+page_start = """
+<html>
+<head>
+<style>
+.dirlink_el {font-size: 125%;}
+.sm_img {max-height: 200px; max-width: 200px;}
+.img_el {display: inline-block; padding-bottom: 15px; padding-right: 10px;}
+</style>
+</head>
+<body>
+"""
+
+page_end = """
+</body>
+</html>
+"""
+
+title_tmplt = """
+<h2>
+{0}
+</h2>
+"""
+
+link_tmplt = """
+<div class="dirlink_el">
+<a href="{0}">{1}</a>
+</div>
+"""
+
+img_tmplt = """
+<div class="img_el">
+<a href={0}><img src="{0}" class="sm_img"></a>
+<br>
+{0}
+<br>
+{1}
+<br>
+{2}
+</div>
+"""
+
 def main():
     # Specify the directory in the command line
     current_directory = os.getcwd()
+    html_content = page_start
     for item in os.scandir(current_directory):
-        print(item)
+        if item.is_file() and item.name.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+            img_info = img_tmplt.format(item.path, item.name, item.stat().st_size)
+            html_content += img_info
+        else:
+            link_info = link_tmplt.format(item.path, item.name)
+            html_content += link_info
+    html_content += page_end
+        
+    print(item)
 
 if __name__ == '__main__':
     main()
