ebf5b34e5bd393c0ff8a19edc3574a79471458aa
HunterKnott <hunterknott00@gmail.com>
2023-10-23 16:58:18 -0600

    Capability to get important exif info from images

--- Indexer/indexer.py
+++ Indexer/indexer.py
@@ -4,6 +4,8 @@ import sys
 import webbrowser
 import http.server
 import socketserver
+import exifread
+import reverse_geocoder
 
 page_start = """
 <html>
@@ -39,6 +41,12 @@ Current Directory: {0}
 </h2>
 """
 
+parent_link_tmplt = """
+<div class="dirlink_el">
+<a href="..">Parent Directory</a>
+</div>
+"""
+
 link_tmplt = """
 <div class="dirlink_el">
 <a href="{0}">{1}</a>
@@ -50,12 +58,9 @@ img_tmplt = """
 <a href={0}><img src="{0}" class="sm_img"></a>
 <br>
 {1}
-<br>
-{2}
 </div>
+<br>
 """
-
-PORT = 8000  # You can choose any available port
 
 class DirectoryViewer(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -66,17 +71,50 @@ def make_html(directory):
     dir_sec = dir_section
     img_sec = img_section
     html_content += title_tmplt.format(directory)
+
     for item in os.scandir(directory):
         if item.is_dir():
             link_info = link_tmplt.format(item.name, item.name)
             dir_sec += link_info
         elif item.is_file() and item.name.endswith(('.jpg', '.jpeg', '.png', '.gif', '.JPG', '.JPEG')):
-            img_info = img_tmplt.format(item.name, item.name, item.stat().st_size)
+            img_info = img_tmplt.format(item.name, item.name)
+
+            exif_data = get_exif_data(item)
+            if exif_data:
+                exif_info = f"EXIF DateTime: {exif_data[0]}<br>"
+                exif_info += f"GPS Latitude Ref: {exif_data[1]}<br>"
+                exif_info += f"GPS Latitude: {exif_data[2]}<br>"
+                exif_info += f"GPS Longitude Ref: {exif_data[3]}<br>"
+                exif_info += f"GPS Longitude: {exif_data[4]}<br>"
+
+                img_info += exif_info
+
             img_sec += img_info
+
     html_content += dir_sec + img_sec + page_end
 
     with open(os.path.join(directory, 'index.html'), 'w') as html_file:
         html_file.write(html_content)
+
+def get_exif_data(image_file):
+    exif_data = {}
+    exif_tags = ["EXIF DateTimeOriginal", "GPS GPSLatitudeRef", "GPS GPSLatitude", "GPS GPSLongitudeRef", "GPS GPSLongitude"]
+    
+    with open(image_file, 'rb') as f:
+        tags = exifread.process_file(f, details=False)
+        for tag in exif_tags:
+            if tag in tags:
+                exif_data[tag] = str(tags[tag])
+    
+    exif_tuple = (
+        exif_data.get("EXIF DateTimeOriginal", ""),
+        exif_data.get("GPS GPSLatitudeRef", ""),
+        exif_data.get("GPS GPSLatitude", ""),
+        exif_data.get("GPS GPSLongitudeRef", ""),
+        exif_data.get("GPS GPSLongitude", "")
+    )
+    
+    return exif_tuple
 
 def main():
     if len(sys.argv) > 1:
@@ -85,13 +123,18 @@ def main():
         root_directory = os.getcwd()
 
     os.chdir(root_directory)
-    
     make_html(root_directory)
 
+    PORT = 8000 
     with socketserver.TCPServer(("", PORT), DirectoryViewer) as httpd:
         print(f"Serving at port {PORT}")
         webbrowser.open(f"http://localhost:{PORT}")
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+        print("Server stopped.")
 
 if __name__ == '__main__':
     main()
