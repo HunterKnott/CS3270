59ec093f9e06f587b157d0d7d151c9c3c486282c
Hunter Knott <hunterknott00@gmail.com>
2023-12-16 12:58:36 -0700

    Added function to search for a word in articles

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import matplotlib.pyplot as plt
 
 # https://developer.nytimes.com/apis for information
@@ -42,10 +43,57 @@ def get_word_count(api_key, year, word):
     plt.grid(True)
     plt.show()
 
+def word_search(api_key, query):
+    url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    params = {
+        "q": query,
+        "api-key": api_key
+    }
+
+    try:
+        # Make the API request
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for bad responses
+        data = response.json()
+
+        # Process and print the results
+        if "response" in data and "docs" in data["response"]:
+            articles = data["response"]["docs"]
+            print(f"Number of articles found: {len(articles)}")
+
+            for article in articles:
+                headline = article["headline"]["main"]
+                date_published = article["pub_date"]
+                print("-" * 30)
+                print(f"Headline: {headline}")
+                print(f"Date Published: {date_published}")
+
+        else:
+            print("No articles found.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making request: {e}")
+
 if __name__ == "__main__":
     api_key = "ejShUxMEpkWI1NDgyz8sZvvjIz7tGgKt"
 
-    year = 2023
-    word = "Israel"
+    while True:
+        print("\nMenu:")
+        print("1. Get word count")
+        print("2. Word Search")
+        print("Q. Quit")
 
-    get_word_count(api_key, year, word)
+        choice = input("Enter a choice of action: ")
+
+        if choice == "1":
+            year = int(input("Enter the year to look at: "))
+            word = input("Enter the word to measure: ")
+            get_word_count(api_key, year, word)
+        elif choice == "2":
+            query = input("Enter the word to search for: ")
+            word_search(api_key, query)
+        elif choice == "q" or "Q":
+            print("Quitting the program.")
+            break
+        else:
+            print("Invalid choice. Please enter a number on the menu.")
